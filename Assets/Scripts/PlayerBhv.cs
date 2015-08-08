d28bd77d9161d5aa85b2e8b929d0d6fb4bf44e73
Sergio Yáñez <sergio.yanez@alumnos.usm.cl>
2015-08-08 20:10:04 -0300

    Player BHV
    Added script for plane behaviour (simple)
    deleted test file

---
+++ Assets/Scripts/PlayerBhv.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBhv : MonoBehaviour {
+
+	public float AmbientSpeed = 300.0f;
+	
+	public float RotationSpeed = 100.0f;
+	
+	void FixedUpdate()
+	{        
+		Quaternion AddRot = Quaternion.identity;
+		float roll = 0;
+		float pitch = 0;
+		float yaw = 0;
+		roll = Input.GetAxis("Horizontal") * (Time.deltaTime * RotationSpeed);
+		pitch = -Input.GetAxis("Vertical") * (Time.deltaTime * RotationSpeed);
+		AddRot.eulerAngles = new Vector3(-pitch, yaw, -roll);
+		GetComponent<Rigidbody>().rotation *= AddRot;
+		Vector3 AddPos = transform.forward;
+		AddPos = GetComponent<Rigidbody>().rotation * AddPos;
+		GetComponent<Rigidbody>().velocity = AddPos * (Time.deltaTime * AmbientSpeed);
+	}
+}
